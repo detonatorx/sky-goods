8c828c26ddda0427b1eeb7349cc432023c709340
alexey.tymchy <offersbox@gmail.com>
2023-07-01 08:07:49 +0300

    browserRouter -> hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BasketPage from './pages/BasketPage';
 import Layout from './components/layout/Layout';
@@ -6,14 +6,14 @@ import React from 'react';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" index element={<HomePage />} />
           <Route path="/basket" element={<BasketPage />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
